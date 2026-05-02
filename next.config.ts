6c8fe116fe2f0ca7b4b9c20b388739c550e8af36
Arian Khademolghorani <ariankhadem4@gmail.com>
2026-05-03 01:27:46 +0700

    Fix deprecred next config

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.ctfassets.net",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
-    domains: ["images.unsplash.com"],
   },
 };
 
